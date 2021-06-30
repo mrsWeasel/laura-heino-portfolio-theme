61391d2c69ad40fa38e4a5392f083eba50b10fbc
Heino Laura <laura.2.heino@sok.fi>
2021-06-30 14:25:58 +0300

    Update timestamp on scripts file to get rid of cache

--- functions.php
+++ functions.php
@@ -172,7 +172,7 @@ function sassyscores_scripts() {
 
 	wp_enqueue_script( 'no-js', get_template_directory_uri() . '/js/no-js.js', array(), '123', false);
 
-	wp_enqueue_script( 'minified-scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '20151215', true );
+	wp_enqueue_script( 'minified-scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '20210630', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
